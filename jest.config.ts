4b4553bee219b9bf9f9cb1900f2eee28df45167a
May Sunktong <pattaranan.sunktong@gmail.com>
2025-08-08 15:24:23 +0200

    Clean config

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,9 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   clearMocks: true,
   coverageProvider: "v8",
@@ -15,5 +13,4 @@ const config: Config = {
   watchPathIgnorePatterns: ["<rootDir>/node_modules/"],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
